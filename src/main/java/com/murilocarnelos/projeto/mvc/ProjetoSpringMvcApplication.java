161b5ba85ba7d2f2a691fd226db68617e523a70b
Murilo Carnelos <muriloc.c@hotmail.com>
2020-07-16 22:25:48 -0300

    Criacao do projeto e classes controller

---
+++ src/main/java/com/murilocarnelos/projeto/mvc/ProjetoSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.murilocarnelos.projeto.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murilocarnelos/projeto/mvc/ProjetoSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilocarnelos.projeto.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
